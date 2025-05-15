59e189e5bdfa801061ac10dd0306aed6d5ef1fdf
Vickler Charles <charlesvikler@gmail.com>
2025-05-16 00:42:18 +0200

    change copy

---
+++ ignition/modules/TokenFactory.ts
@@ -0,0 +1,18 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
+
+const TokenFactoryModule = buildModule('TokenFactoryModule', (m) => {
+  // First deploy the Coupon implementation contract
+  const couponImpl = m.contract('Coupon');
+
+  // Deploy the TokenFactory with the Coupon implementation address
+  const tokenFactory = m.contract('TokenFactory', [couponImpl]);
+
+  // Return both deployed contracts
+  return { couponImpl, tokenFactory };
+});
+
+export default TokenFactoryModule;
+
